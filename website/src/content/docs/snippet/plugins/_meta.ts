f56cad0dd3a7bb8530509d4ee17f6b5fc1532c65
Bob Singor <bob@singor.com>
2025-12-18 13:41:28 +0200

    Add document manager example

--- website/src/content/docs/snippet/plugins/_meta.ts
+++ website/src/content/docs/snippet/plugins/_meta.ts
@@ -1,4 +1,5 @@
 export default {
+  'plugin-document-manager': 'Loading & Documents',
   'plugin-i18n': 'Internationalization',
   'plugin-scroll': 'Scrolling & Navigation',
   'plugin-export': 'Export',
