b9fc0cabde4d4063c13336128ba0022a8b4be3ab
Bob Singor <bob@singor.com>
2025-12-21 16:03:58 +0200

    Bug fix that caused issues clicking the button on the homepage

--- website/src/components/icons/javascript.tsx
+++ website/src/components/icons/javascript.tsx
@@ -1,12 +1,13 @@
-export const JavaScript = () => {
+export const JavaScript = (props: React.ComponentProps<'svg'>) => {
   return (
     <svg
-      width="512px"
-      height="512px"
+      width="100%"
+      height="100%"
       viewBox="0 0 512 512"
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
+      {...props}
     >
       <g
         id="Page-1"
