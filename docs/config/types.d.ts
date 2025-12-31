d49ed0058dfa016a077a25b7352c62d2c491a83d
menglb <lingbinmeng@hotmail.com>
2025-12-31 21:10:45 +0800

    添加发布文件。

---
+++ docs/config/types.d.ts
@@ -0,0 +1,6 @@
+import { GlobalStoreState } from '@embedpdf/core';
+import { UI_PLUGIN_ID, UIState } from '@embedpdf/plugin-ui';
+export type State = GlobalStoreState<{
+    [UI_PLUGIN_ID]: UIState;
+}>;
+//# sourceMappingURL=types.d.ts.map
