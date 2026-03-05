6eccf82a07e3998b1a531913d28b5285f54f7e48
Bob Singor <bob@singor.com>
2026-03-05 22:54:32 +0200

    Add caret support to vue

---
+++ packages/plugin-annotation/src/vue/components/renderers/caret-renderer.vue
@@ -0,0 +1,17 @@
+<template>
+  <CaretVue
+    v-bind="currentObject"
+    :scale="scale"
+    :onClick="onClick"
+    :appearanceActive="appearanceActive"
+  />
+</template>
+
+<script setup lang="ts">
+import { PdfCaretAnnoObject } from '@embedpdf/models';
+import { AnnotationRendererProps } from '../../context/types';
+import CaretVue from '../annotations/caret.vue';
+
+const { currentObject, scale, onClick, appearanceActive } =
+  defineProps<AnnotationRendererProps<PdfCaretAnnoObject>>();
+</script>
